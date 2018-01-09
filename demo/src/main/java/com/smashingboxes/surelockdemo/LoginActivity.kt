1d95e78809802088c9c417c189d34081c4673e5b
souvenotes <nicholascook@Nicholass-MacBook-Pro-2.local>
2018-01-09 11:55:39 -0500

    Code review fixes

--- demo/src/main/java/com/smashingboxes/surelockdemo/LoginActivity.kt
+++ demo/src/main/java/com/smashingboxes/surelockdemo/LoginActivity.kt
@@ -4,19 +4,20 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.content.SharedPreferences
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.smashingboxes.surelock.*
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import java.io.UnsupportedEncodingException
 
 /**
@@ -30,18 +31,20 @@ import java.io.UnsupportedEncodingException
 
 class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
 
-    private var authTask: UserLoginTask? = null
-    private var surelock: Surelock? = null
     private lateinit var surelockStorage: SurelockStorage
-    private var preferences: SharedPreferences? = null
-
-    private val preferenceHelper: SharedPreferences
-        get() {
-            if (preferences == null) {
-                preferences = getSharedPreferences(SHARED_PREFS_FILE_NAME, Context.MODE_PRIVATE)
-            }
-            return preferences!!
-        }
+    private val surelock: Surelock by lazy {
+        Surelock.Builder(this)
+            .withDefaultDialog(R.style.SurelockDemoDialog)
+            .withKeystoreAlias(KEYSTORE_KEY_ALIAS)
+            .withFragmentManager(fragmentManager)
+            .withSurelockFragmentTag(FINGERPRINT_DIALOG_FRAGMENT_TAG)
+            .withSurelockStorage(surelockStorage)
+            .build()
+    }
+    private val preferenceHelper: SharedPreferences by lazy {
+        getSharedPreferences(SHARED_PREFS_FILE_NAME, Context.MODE_PRIVATE)
+    }
+    private var authJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,19 +63,20 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
                 false
             })
 
-        val signInButton = findViewById<View>(R.id.sign_in_button) as Button
-        signInButton.setOnClickListener { handleLoginClick() }
+        sign_in_button.setOnClickListener { handleLoginClick() }
     }
 
     override fun onResume() {
         super.onResume()
+        showProgress(false)
         if (userWantsToUseFingerprintToLogin()) {
             if (!Surelock.fingerprintAuthIsSetUp(this, true)) {
                 fingerprint_checkbox.visibility = View.VISIBLE
                 fingerprint_checkbox.isChecked = false
+                surelockStorage.clearAll()
             } else {
                 try {
-                    getSurelock().loginWithFingerprint(KEY_CRE_DEN_TIALS)
+                    surelock.loginWithFingerprint(KEY_CRE_DEN_TIALS)
                 } catch (e: SurelockInvalidKeyException) {
                     surelockStorage.clearAll()
                     showFingerprintLoginInvalidated()
@@ -84,29 +88,16 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
     }
 
     override fun onStop() {
-        //Clear storage so users don't have to clear app data to try out new credentials
-        surelockStorage.clearAll()
+        authJob?.cancel()
         super.onStop()
     }
 
-    private fun getSurelock(): Surelock {
-        if (surelock == null) {
-            surelock = Surelock.Builder(this)
-                .withDefaultDialog(R.style.SurelockDemoDialog)
-                .withKeystoreAlias(KEYSTORE_KEY_ALIAS)
-                .withFragmentManager(fragmentManager)
-                .withSurelockFragmentTag(FINGERPRINT_DIALOG_FRAGMENT_TAG)
-                .withSurelockStorage(surelockStorage)
-                .build()
-        }
-        return surelock!!
-    }
-
     private fun showFingerprintLoginInvalidated() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle(R.string.sl_login_error)
+        AlertDialog.Builder(this)
+            .setTitle(R.string.sl_login_error)
             .setMessage(R.string.sl_re_enroll)
-            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }.show()
+            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+            .show()
     }
 
     private fun userWantsToUseFingerprintToLogin(): Boolean {
@@ -117,7 +108,7 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
         var errorsExist = false
         var focusView: View? = null
 
-        if (TextUtils.isEmpty(usernameInput)) {
+        if (usernameInput.isNullOrEmpty()) {
             username_input_layout.error = getString(R.string.error_field_required)
             focusView = username
             errorsExist = true
@@ -125,7 +116,7 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
             username_input_layout.error = null
         }
 
-        if (TextUtils.isEmpty(passwordInput)) {
+        if (passwordInput.isNullOrEmpty()) {
             password_input_layout.error = getString(R.string.error_field_required)
             focusView = password
             errorsExist = true
@@ -152,42 +143,52 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
 
     private fun attemptLogin(usernameInput: String?, passwordInput: String?) {
         // Don't allow multiple login attempts in a row
-        if (authTask != null) {
-            return
-        }
+        if (authJob == null || authJob?.isActive == false) {
+            if (!validateLoginForm(usernameInput, passwordInput)) {
+                showProgress(true)
 
-        if (!validateLoginForm(usernameInput, passwordInput)) {
-            showProgress(true)
-
-            authTask = UserLoginTask(false)
-            //We're just fetching this to show you on the next screen for demo purposes.
-            var storedEncryptedValueString = ""
-            try {
-                val storedEncryptedValue = surelockStorage.get(KEY_CRE_DEN_TIALS)
-                if (storedEncryptedValue != null) {
-                    storedEncryptedValueString = String(storedEncryptedValue, Charsets.UTF_8)
+                //We're just fetching this to show you on the next screen for demo purposes.
+                var storedEncryptedValueString = ""
+                try {
+                    val storedEncryptedValue = surelockStorage.get(KEY_CRE_DEN_TIALS)
+                    if (storedEncryptedValue != null) {
+                        storedEncryptedValueString = String(storedEncryptedValue, Charsets.UTF_8)
+                    }
+                } catch (e: UnsupportedEncodingException) {
+                    Log.d(TAG, "attemptLogin: cannot fetch encrypted credentials")
                 }
-            } catch (e: UnsupportedEncodingException) {
-                Log.d(TAG, "attemptLogin: cannot fetch encrypted credentials")
-            }
 
-            authTask!!.execute(
-                username.text.toString(), password.text.toString(),
-                storedEncryptedValueString,
-                usernameInput, passwordInput)
+                authJob = launch(UI) {
+                    delay(2000)
+                    MainActivity.start(this@LoginActivity, username.text.toString(),
+                        password.text.toString(), storedEncryptedValueString, usernameInput ?: "",
+                        passwordInput ?: "")
+                    showProgress(false)
+                }
+            }
         }
     }
 
     private fun attemptLoginForFingerprintEnrollment(usernameInput: String, passwordInput: String) {
         // Don't allow multiple login attempts in a row
-        if (authTask != null) {
-            return
-        }
-
-        if (!validateLoginForm(usernameInput, passwordInput)) {
-            showProgress(true)
-            authTask = UserLoginTask(true)
-            authTask!!.execute(username.text.toString(), password.text.toString())
+        if (authJob == null || authJob?.isActive == false) {
+            if (!validateLoginForm(usernameInput, passwordInput)) {
+                showProgress(true)
+                authJob = launch(UI) {
+                    delay(2000)
+                    try {
+                        surelock.enrollFingerprintAndStore(KEY_CRE_DEN_TIALS,
+                            getFormattedCredentialsForEncryption(usernameInput, passwordInput))
+                    } catch (e: UnsupportedEncodingException) {
+                        Toast.makeText(this@LoginActivity, "Failed to encrypt the login",
+                            Toast.LENGTH_LONG).show()
+                        Log.e(TAG, "Failed to encrypt the login" + e.message)
+                    } catch (e: SurelockException) {
+                        Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                    showProgress(false)
+                }
+            }
         }
     }
 
@@ -214,56 +215,6 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
                     login_progress.visibility = if (show) View.VISIBLE else View.GONE
                 }
             })
-    }
-
-    /**
-     * Represents an asynchronous login/registration task
-     * used to authenticate the user.
-     */
-    private inner class UserLoginTask(private val withFingerprintEnrollment: Boolean) :
-        AsyncTask<String, Void, Array<String>>() {
-
-        override fun doInBackground(vararg params: String): Array<String> {
-            try {
-                // You should add your network call to authenticate here.
-                // This just "simulates" network access.
-                Thread.sleep(2000)
-            } catch (e: InterruptedException) {
-                Log.e(TAG, "doInBackground: auth task failed", e)
-            }
-
-            return if (withFingerprintEnrollment) {
-                arrayOf(params[0], params[1])
-            } else {
-                arrayOf(params[0], params[1], params[2], params[3], params[4])
-            }
-        }
-
-        override fun onPostExecute(params: Array<String>) {
-            authTask = null
-            showProgress(false)
-            if (withFingerprintEnrollment) {
-                try {
-                    getSurelock().enrollFingerprintAndStore(KEY_CRE_DEN_TIALS,
-                        getFormattedCredentialsForEncryption(params[0], params[1]))
-                } catch (e: UnsupportedEncodingException) {
-                    Toast.makeText(this@LoginActivity, "Failed to encrypt the login",
-                        Toast.LENGTH_LONG).show()
-                    Log.e(TAG, "Failed to encrypt the login" + e.message)
-                } catch (e: SurelockException) {
-                    Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_LONG).show()
-                }
-
-            } else {
-                MainActivity.start(this@LoginActivity, params[0], params[1], params[2], params[3],
-                    params[4])
-            }
-        }
-
-        override fun onCancelled() {
-            authTask = null
-            showProgress(false)
-        }
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -294,7 +245,7 @@ class LoginActivity : AppCompatActivity(), SurelockFingerprintListener {
 
         try {
             val decryptedString = String(decryptedValue, Charsets.UTF_8)
-            if (!TextUtils.isEmpty(decryptedString)) {
+            if (!decryptedString.isEmpty()) {
                 username = getUsernameFromFormattedCredentials(decryptedString)
                 password = getPasswordFromFormattedCredentials(decryptedString)
             }
